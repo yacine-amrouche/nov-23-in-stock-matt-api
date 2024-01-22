09bc701f491f4afbc51be093dde4120a5c5d0aa2
tomas-tank3ngine <tomasmartinez1424@gmail.com>
2024-01-22 14:17:47 -0500

    add updates to individual items and all inventory items requests

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,51 +1,28 @@
 const knexConfig = require("../knexfile.js").development;
 const knex = require("knex")(knexConfig);
 
-// //Don't think we need this -Tomas
-// const index = async (_req, res) => {
-//   try {
-//     const data = await knex("inventories");
-//     res.status(200).json(data);
-//   } catch (err) {
-//     res.status(400).send(`Error retrieving Users: ${err}`);
-//   }
-// };
-
-//Retrieve all inventory items from all warehouses
-//converts each warehouse id into the warehouse's name
-//This can probably be done with knex join
 const allInventoryItems = async (_req, res) => {
-  const warehouseIdToName = async (warehouse_id) => {
-    const warehouse = await knex("warehouses")
-      .where({ id: warehouse_id })
-      .first();
-    return warehouse ? warehouse.warehouse_name : null;
-  };
-
   try {
-    const data = await knex("inventories");
-
-    //Wait for all other promises, then
-    //create a new array with inventory items as objects
-    const invWithWarehouseNames = await Promise.all(
-      data.map(async (item) => {
-        const { id, warehouse_id, ...rest } = item; //destructure to separate warehouse_id from list item key/values
-        const warehouse_name = await warehouseIdToName(item.warehouse_id);
-
-        //remake the inventory list object in order
-        return {
-          id,
-          warehouse_name,
-          ...rest,
-        };
-      })
-    );
+    const invWithWarehouseNames = await knex.select(
+      'inventories.id',
+      'warehouses.warehouse_name',
+      'inventories.item_name',
+      'inventories.description',
+      'inventories.category',
+      'inventories.status',
+      'inventories.quantity',
+    )
+    .from('inventories')
+    .join('warehouses', 'inventories.warehouse_id', 'warehouses.id');
 
     res.status(200).json(invWithWarehouseNames);
   } catch (error) {
-    console.log("error at inventory controller allInventoryItems: ", error);
+    console.error("Error at inventory controller allInventoryItems: ", error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+
 
 const remove = async (req, res) => {
   try {
@@ -127,15 +104,33 @@ const update = async (req, res) => {
 };
 
 const getIndividual = async (req, res) => {
+  const warehouseIdToName = async (warehouse_id) => {
+    const warehouse = await knex("warehouses")
+      .where({ id: warehouse_id })
+      .first();
+    return warehouse ? warehouse.warehouse_name : null;
+  };
   try {
-    const usersFound = await knex("inventories").where({ id: req.params.id });
-    if (usersFound.length === 0) {
-      return res.status(404).json({
-        message: `User with ID ${req.params.id} not found`,
-      });
+    //get a single inventory item based on it's id
+    const itemFound = await knex("inventories")
+      .where({ id: req.params.id })
+      .first();
+    //If the item is not found, respond appropriately
+    if (!itemFound) {
+      return res
+        .status(404)
+        .json({ message: `User with ID ${req.params.id} not found` });
     }
-    const userData = usersFound[0];
-    res.json(userData);
+    //Retrieve the warehouse name asynchronously
+    //has to be awaited because 'warehouseIdToName' is an async funct (line 174-177)
+    const warehouse_name = await warehouseIdToName(itemFound.warehouse_id);
+    //create a new object with all the info we need: the id, the name, and the rest of itemFound's key/value pairs
+    const itemWithWarehouseName = {
+      id: itemFound.id,
+      warehouse_name: warehouse_name,
+      ...itemFound,
+    };
+    res.json(itemWithWarehouseName);
   } catch (error) {
     res.status(500).json({
       message: `Unable to retrieve user data for user with ID ${req.params.id}`,
